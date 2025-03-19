0669d2c1fc910ef0e5e11343551b38bcbc52ac83
unknown <1820ayala@gmail.con>
2025-03-19 19:40:49 +0200

    add all files of the project

---
+++ src/service.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const apiUrl = "http://localhost:5141"
+
+export default {
+  getTasks: async () => {
+
+    try {
+      
+      
+    const result = await axios.get(`${apiUrl}/items`)
+    return result.data;// מחזיר את הפריט שנוצר
+    } catch (error) {
+      console.error('Error get items:', error);
+      throw error; // מטפל בשגיאות
+    }
+  },
+
+  addTask: async (name) => {
+    console.log('addTask', name)
+    try {
+      
+      const response = await axios.post(`${apiUrl}/items/creatItem${name}`);
+      return response.data; // מחזיר את הפריט שנוצר
+    } catch (error) {
+      console.error('Error creating item:', error);
+      throw error; // מטפל בשגיאות
+    }
+  },
+
+  setCompleted: async (id, isComplete) => {
+    console.log('setCompleted', { id, isComplete })
+    try {
+      
+      const result = await axios.put(`${apiUrl}/items/${id}`, null, {
+        params: { IsComplete: isComplete },
+      });
+    } catch (error) {
+      console.error('Error set item:', error);
+      throw error; // מטפל בשגיאות
+    }
+   
+
+    return {};
+  },
+
+  deleteTask: async (id) => {
+
+    console.log('deleteTask')
+    try {
+      await axios.delete(`${apiUrl}/items/${id}`);
+      return true; // מחזיר true אם המחיקה הצליחה
+    } catch (error) {
+      console.error('Error deleting item:', error);
+      throw error; // מטפל בשגיאות
+    }
+  }
+};
